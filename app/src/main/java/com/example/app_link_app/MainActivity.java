ce1c8d9138826ba50c3290d93840d6b305306c40
MatthewSaw <sawsc94@gmail.com>
2020-10-16 17:39:15 +0800

    Minor updates

--- app/src/main/java/com/example/app_link_app/MainActivity.java
+++ app/src/main/java/com/example/app_link_app/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.app_link_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent intent = getIntent();
+        String appLinkAction = intent.getAction();
+        Uri appLinkData = intent.getData();
+        
+        Log.d("MainActivity", appLinkAction);
+        if (appLinkData != null) {
+            String id = intent.getStringExtra("SESSION_ID");
+            textView.setText(id);
+        }
     }
 }
